2490da46c2c1d8bd1830c6448507a4f1a5d84db2
Boaz Halter <30419068+BoazHalter@users.noreply.github.com>
2023-11-28 13:26:16 +0200

    Update main.tf

--- main.tf
+++ main.tf
@@ -5,7 +5,8 @@ provider "aws" {
   resource "aws_vpc" "company1_vpc" {
     cidr_block = "10.0.0.0/16"
   }
-  
+
+
   resource "aws_subnet" "company1_subnet" {
     vpc_id     = aws_vpc.company1_vpc.id
     cidr_block = "10.0.1.0/24"
